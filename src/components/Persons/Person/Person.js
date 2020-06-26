f51095771b9a8b7da5cabb837a7dde3dcd3fe271
Ruturaj More <ruturajmore1234@gmail.com>
2020-06-26 23:50:41 +0530

    master: 92. Component Update Lifecycle (for props Changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,43 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.module.css';
-import styled from 'styled-components'
 
-const StyledDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eee;
-  box-shadow: 0 2px 3px #ccc;
-  padding: 16px;
-  text-align: center;
-  @media (min-width: 500px){
-    width: 450px;
+class Person extends Component {
+  render(){
+    console.log("[Person.js] render");
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old person!</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.change} value={this.props.name}/>
+      </div>
+    );
   }
-  `;
-
-const person = (props) => {
-  console.log("[Person.js] render");
-
-  // const style = {
-  //   '@media (min-width: 500px)': {
-  //     width: '450px'
-  //   }
-  // };
-
-  // const rnd = Math.random();
-  // if(rnd > 0.7){
-  //   throw new Error('Something went wron!');
-  // }
-
-  return (
-    // <div className="Person" style={style}>
-    // <StyledDiv>
-    <div className={classes.Person}>
-      <p onClick={props.click}>I am {props.name} and I am {props.age} years old person!</p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.change} value={props.name}/>
-    </div>
-    // </StyledDiv>
-  )
 }
 
-export default person;
+export default Person;
