63cbf5b8ed4e4bdb950b006b8aa0d270ed91f7f1
Ruturaj More <ruturajmore1234@gmail.com>
2020-06-28 18:08:42 +0530

    master: 105. Using React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.module.css';
 import Aux from '../../../hoc/Aux.js'
 
@@ -6,11 +6,11 @@ class Person extends Component {
   render(){
     console.log("[Person.js] render");
     return (
-      <Aux>  {/* className={classes.Person} it didn't got applied*/}
+      <Fragment>  {/* className={classes.Person} it didn't got applied*/}
         <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old person!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.change} value={this.props.name}/>
-      </Aux>
+      </Fragment>
     );
   }
 }
